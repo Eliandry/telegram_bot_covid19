0f68d12c37b62aaebbd1a534b04dac4e3484a302
andrey <60395119+Eliandry@users.noreply.github.com>
2020-04-01 21:02:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,58 @@
-import COVID19Py
 import telebot
 from telebot import types
+import COVID19Py
 
-covid=COVID19Py.COVID19()
-bot=telebot.TeleBot('1229612694:AAEM0N0LJfTKf8oM0SSSJJ4ErjtrJYYQedE')
+covid19 = COVID19Py.COVID19()
+bot = telebot.TeleBot('ВАШ_API_КЛЮЧ')
+
 @bot.message_handler(commands=['start'])
 def start(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    btn1 = types.KeyboardButton('Во всём мире')
-    btn2 = types.KeyboardButton('Украина')
-    btn3 = types.KeyboardButton('Россия')
-    btn4 = types.KeyboardButton('Беларусь')
-    markup.add(btn1, btn2, btn3, btn4)
-    send=f"<b>Привет {message.from_user.first_name}!</b>\nВведите страну"
-    bot.send_message(message.chat.id,send,parse_mode='html',reply_markup=markup)
+	markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+	btn1 = types.KeyboardButton('Во всём мире')
+	btn2 = types.KeyboardButton('Украина')
+	btn3 = types.KeyboardButton('Россия')
+	btn4 = types.KeyboardButton('Беларусь')
+	markup.add(btn1, btn2, btn3, btn4)
+
+	send_message = f"<b>Привет {message.from_user.first_name}!</b><br>Чтобы узнать данные про коронавируса напишите " \
+		f"название страны, например: США, Украина, Россия и так далее<br><br>Заходи к нам на сайт <a href='https://itproger.com'>itProger</a>"
+	bot.send_message(message.chat.id, send_message, parse_mode='html', reply_markup=markup)
 
 @bot.message_handler(content_types=['text'])
 def mess(message):
-    final=""
-    get_message=message.text.strip().lower()
-    if get_message == "россия":
-        location=covid.getLocationByCountryCode("RU")
-    elif get_message == "украина":
-        location=covid.getLocationByCountryCode("UA")
-    elif get_message == "сша":
-        location=covid.getLocationByCountryCode("US")
-    elif get_message == "беларусь":
-        location = covid.getLocationByCountryCode("BY")
-    elif get_message == "италия":
-        location=covid.getLocationByCountryCode("IT")
-    else:
-        location= covid.getLatest()
-        final = f"<u>Данные по всему миру:</u><br><b>Заболевших:" \
-                f" </b>{location['confirmed']:,}<br>" \
-                f"<b>Сметрей: </b>{location['deaths']:,}"
-    if final == "":
-        date = location[0]['last_updated'].split("T")
-        time = date[1].split(".")
-        final= f"<u>Данные по стране:</u><br>Население: {location[0]['country_population']:,}<br>" \
-                        f"Последнее обновление: {date[0]} {time[0]}<br>Последние данные:<br><b>" \
-                        f"Заболевших: </b>{location[0]['latest']['confirmed']:,}<br><b>Сметрей: </b>" \
-                        f"{location[0]['latest']['deaths']:,}"
-    bot.send_message(message.chat.id, final,parse_mode='html')
+	final_message = ""
+	get_message_bot = message.text.strip().lower()
+	if get_message_bot == "сша":
+		location = covid19.getLocationByCountryCode("US")
+	elif get_message_bot == "украина":
+		location = covid19.getLocationByCountryCode("UA")
+	elif get_message_bot == "россия":
+		location = covid19.getLocationByCountryCode("RU")
+	elif get_message_bot == "беларусь":
+		location = covid19.getLocationByCountryCode("BY")
+	elif get_message_bot == "казакхстан":
+		location = covid19.getLocationByCountryCode("KZ")
+	elif get_message_bot == "италия":
+		location = covid19.getLocationByCountryCode("IT")
+	elif get_message_bot == "франция":
+		location = covid19.getLocationByCountryCode("FR")
+	elif get_message_bot == "германия":
+		location = covid19.getLocationByCountryCode("DE")
+	elif get_message_bot == "япония":
+		location = covid19.getLocationByCountryCode("JP")
+	else:
+		location = covid19.getLatest()
+	final_message = f"<u>Данные по всему миру:</u><br><b>Заболевших: </b>{location['confirmed']:,}<br><b>Сметрей: </b>{location['deaths']:,}"
+
+	if final_message == "":
+		date = location[0]['last_updated'].split("T")
+		time = date[1].split(".")
+		final_message = f"<u>Данные по стране:</u><br>Население: {location[0]['country_population']:,}<br>" \
+				f"Последнее обновление: {date[0]} {time[0]}<br>Последние данные:<br><b>" \
+				f"Заболевших: </b>{location[0]['latest']['confirmed']:,}<br><b>Сметрей: </b>" \
+				f"{location[0]['latest']['deaths']:,}"
+
+	bot.send_message(message.chat.id, final_message, parse_mode='html')
+
+
 bot.polling(none_stop=True)
